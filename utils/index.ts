01b35cf08fae3d7519ce587487ec65497ac79cce
youyi.qin <youyi.qin@shihengtech.com>
2022-05-03 00:13:37 +0800

    feat: :tada: add utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,36 +1,41 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { marked } from "marked";
 
 const fsPromise = fs.promises;
+const rootPath = process.cwd();
 
-export const getAllPostData = async () => {
+export const getAllPostArticles = async () => {
   try {
-    const rootPath = process.cwd();
     const basePath = path.join(rootPath, "posts");
     const dirs = await fsPromise.readdir(basePath);
     // sort by date
     dirs.sort();
-    const records: { [key: string]: {} } = {};
-    dirs.forEach(async (dir) => {
+    const records: {
+      [key: string]: {
+        articlePath: string;
+        parentDir: string;
+      };
+    } = {};
+    dirs.forEach((dir) => {
       // read md files and save data;
       const dateDirPath = path.join(basePath, dir);
       if (fs.lstatSync(dateDirPath).isDirectory()) {
-        const posts = await fsPromise.readdir(dateDirPath);
+        const posts = fs.readdirSync(dateDirPath);
         posts
           .filter(
             (i) =>
               fs.lstatSync(path.join(dateDirPath, i)).isFile() &&
               i.endsWith(".md")
           )
-          .forEach(async (article) => {
+          .forEach((article) => {
             // read article and save data
             const articlePath = path.join(dateDirPath, article);
-            const rawContent = await fsPromise.readFile(articlePath, "utf-8");
-            const { data, content } = matter(rawContent);
-            console.log(content);
-
-            records[dir] = { ...data, content };
+            records[article] = {
+              articlePath,
+              parentDir: dir,
+            };
           });
       }
     });
@@ -39,3 +44,26 @@ export const getAllPostData = async () => {
     console.log(error);
   }
 };
+
+export const getAllPostMetadata = async () => {
+  try {
+    const articles = await getAllPostArticles();
+    if (!articles) return [];
+    const records: { [key: string]: {} } = {};
+    const tasks = Object.keys(articles).map(async (article) => {
+      const { articlePath, parentDir } = articles[article];
+      const rawContent = await fsPromise.readFile(articlePath, "utf-8");
+      const { data } = matter(rawContent);
+      records[parentDir] = {
+        ...data,
+        weeklyName: article.replace(/\.md$/, ""),
+      };
+    });
+    await Promise.all(tasks);
+    return records;
+  } catch (error) {
+    console.log("get all post metadata failed!!!");
+  }
+};
+
+const getArticleHtml = async (slug: string) => {};
